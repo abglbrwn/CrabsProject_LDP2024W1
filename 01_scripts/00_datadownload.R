2b400405bed52a90fc834f3d4eb787c64fffc8a3
Abi Brown <abibrown@dhcp-128-189-209-5.ubcsecure.wireless.ubc.ca>
2024-09-17 16:23:20 -0700

    new data download for crabs

--- 01_scripts/00_datadownload.R
+++ 01_scripts/00_datadownload.R
@@ -1,23 +1,7 @@
-#install BIEN, sf, and tidyverse if not already installed
-install.packages("BIEN")
-install.packages("sf")
-install.packages("tidyverse")
+##The data to download is the crabs data accessed via the MASS package
+library(MASS)
 
-library(tidyverse)
-library(sf)
-library(BIEN)
+crabs_data <- MASS::crabs
 
-#from BIEN (Botanical Information and Ecology Network) database, 
-#download range maps for common British Columbia conifer species
-
-Conifers <- c("Abies amabilis","Abies grandis", "Abies lasiocarpa", "Thuja plicata",
-              "Pseudotsuga menziesii", "Tsuga mertensiana","Tsuga heterophylla",
-              "Juniperus scopulorum", "Larix lyallii","Larix laricina", "Larix occidentalis",
-              "Pinus albicaulis","Pinus flexilis","Pinus banksiana","Pinus contorta",
-              "Pinus monticola","Pinus ponderosa","Picea mariana","Picea engelmannii",
-              "Picea sitchensis","Picea glauca", "Taxus brevifolia","Chamaecyparis nootkatensis")
-
-Conifer_ranges <- BIEN_ranges_load_species(Conifers)
-
-#this will write the ranges as a singular shapefile in the rawdata folder
-st_write(Conifer_ranges, "../Project_LDP2024W1/00_rawdata/Conifer_ranges.shp")
+#the data is exported as a csv
+write.csv(crabs_data, "../Project_LDP2024W1/00_rawdata/crabs_measurements.csv")
